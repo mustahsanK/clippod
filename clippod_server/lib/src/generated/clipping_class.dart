21f9c65c362a37a5af5ba4fa50e6f970056fb315
mustahsanK <mustahsan60@outlook.com>
2023-03-07 23:47:40 +0530

    Added Clipping Class

---
+++ clippod_server/lib/src/generated/clipping_class.dart
@@ -0,0 +1,59 @@
+/* AUTOMATICALLY GENERATED CODE DO NOT MODIFY */
+/*   To generate run: "serverpod generate"    */
+
+// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: public_member_api_docs
+// ignore_for_file: implementation_imports
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:serverpod/serverpod.dart' as _i1;
+
+class Clipping extends _i1.SerializableEntity {
+  Clipping({
+    required this.uid,
+    required this.data,
+    required this.source,
+    required this.date,
+  });
+
+  factory Clipping.fromJson(
+    Map<String, dynamic> jsonSerialization,
+    _i1.SerializationManager serializationManager,
+  ) {
+    return Clipping(
+      uid: serializationManager.deserialize<Integer>(jsonSerialization['uid']),
+      data: serializationManager.deserialize<String>(jsonSerialization['data']),
+      source:
+          serializationManager.deserialize<String>(jsonSerialization['source']),
+      date: serializationManager.deserialize<String>(jsonSerialization['date']),
+    );
+  }
+
+  Integer uid;
+
+  String data;
+
+  String source;
+
+  String date;
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'uid': uid,
+      'data': data,
+      'source': source,
+      'date': date,
+    };
+  }
+
+  @override
+  Map<String, dynamic> allToJson() {
+    return {
+      'uid': uid,
+      'data': data,
+      'source': source,
+      'date': date,
+    };
+  }
+}
